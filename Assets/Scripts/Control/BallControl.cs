b24eb43628d15e2e6f48b95a2ea5843f0a85abfa
Amirrulloh Azis <amirulloh.azis@gmail.com>
2023-10-29 20:23:03 +0700

    Adding Ball Physic, New Tag, Add Barrier

--- Assets/Scripts/Control/BallControl.cs
+++ Assets/Scripts/Control/BallControl.cs
@@ -5,12 +5,33 @@ using UnityEngine;
 public class BallControl : MonoBehaviour
 {
     [SerializeField] bool isCarrying;
+    [SerializeField] float force;
+    Rigidbody rb;
 
-    void OnTriggerStay(Collider other) {
+    void Start() {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void Update() {
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            isCarrying = false;
+            rb.AddForce(transform.rotation * Vector3.forward * force, ForceMode.Impulse);
+        }
+
+        if (isCarrying)
+        {
+            this.transform.position = FindObjectOfType<BallPoint>().transform.position;
+            this.transform.rotation = FindObjectOfType<BallPoint>().transform.rotation;
+        }
+    }
+
+
+
+    void OnTriggerEnter(Collider other) {
         Debug.Log("collide " + other.gameObject.tag);
         if (other.gameObject.tag == "Attacker")
         {
-            this.transform.position = FindObjectOfType<BallPoint>().transform.position;
             isCarrying = true;
         }
     }
